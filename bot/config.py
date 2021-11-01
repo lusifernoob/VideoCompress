9b1cc7a3ce223d7a7ea071442cf3a24633fa5c65
LUSIFER <70184956+lusifernoob@users.noreply.github.com>
2021-11-01 13:48:57 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -37,8 +37,8 @@ class Config(object):
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
     # add config vars for the display progress
-    FINISHED_PROGRESS_STR = get_config("FINISHED_PROGRESS_STR", "▓")
-    UN_FINISHED_PROGRESS_STR = get_config("UN_FINISHED_PROGRESS_STR", "░")
+    FINISHED_PROGRESS_STR = get_config("FINISHED_PROGRESS_STR", "⭐")
+    UN_FINISHED_PROGRESS_STR = get_config("UN_FINISHED_PROGRESS_STR", "🌟")
     LOG_FILE_ZZGEVC = get_config("LOG_FILE_ZZGEVC", "Log.txt")
       # because, https://t.me/c/1494623325/5603
     SHOULD_USE_BUTTONS = get_config("SHOULD_USE_BUTTONS", False)
